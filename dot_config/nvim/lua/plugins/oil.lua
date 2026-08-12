7f312241af509df95025a13afda2c0a07953f378
Adryan Almeida <adryan1990@gmail.com>
2026-08-11 21:15:44 -0300

    Rebuild chezmoi for CachyOS + macOS split.
    Drop dead Mac WM/mux stacks and full LazyVim tree; track Linux desktop
    configs, fish+mise, and nvim overlays with a LazyVim starter bootstrap.

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      columns = {
+        "icon",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+}
